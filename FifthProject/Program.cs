7fe01151a5195200975c8b0a6e917430c459cc8f
Ankit <ankitcool4370@gmail.com>
2022-11-06 02:41:57 -0800

    welcome

---
+++ FifthProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int temp;
+                      temp = a;
+            Console.WriteLine("Swapped Value of A= " + b);
+            Console.ReadLine();
+            Console.WriteLine("Swapped value of B= " + a);
+            Console.ReadLine(); 
+
+        }
+    }
+}
